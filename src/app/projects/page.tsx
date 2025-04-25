47c61d28de47c8ccd337f8e099f3a779a6e94987
balumartin <martinka.balazs@gmail.com>
2025-04-25 17:57:06 +0200

    work in progress

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,13 @@
+export default function Projects() {
+  return (
+    <section className="mx-container">
+      <div className="b-container">
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
