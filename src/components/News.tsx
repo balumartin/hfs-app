c55a06a34e102c1d1f6ef3c862592f45c4cb27ec
balumartin <martinka.balazs@gmail.com>
2025-04-25 18:00:19 +0200

    add News component

---
+++ src/components/News.tsx
@@ -0,0 +1,21 @@
+import { NEWS } from "@/constants";
+import Card from "./Card";
+
+export default function News() {
+  return (
+    <section className="mx-container my-10">
+      <h2 className="pt-10 bold-30 sr-only">Legfrissebb hírek</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+        {NEWS.map((n) => (
+          <Card
+            key={n.key}
+            autor={n.autor}
+            title={n.title}
+            subtitle={n.subtitle}
+            backgroundImg={n.backgroundImg}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
