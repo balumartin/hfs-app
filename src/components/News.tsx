42ac6630afb55381e2597df8bb6b89087bfe3595
balumartin <martinka.balazs@gmail.com>
2025-04-28 10:52:24 +0200

    fix margin

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 
 export default function News() {
   return (
-    <section className="mx-container my-10">
+    <section className="mx-container mb-10">
       <h2 className="pt-10 bold-30 sr-only">Legfrissebb hírek</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
         {NEWS.map((n) => (
