ca7784d68dd683a1c407fee4118d5edbb77fcccc
balumartin <martinka.balazs@gmail.com>
2025-05-15 14:06:14 +0200

    add font-styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#E74F1C",
+        secondary: "#F4F4F4",
+        foreground: "#1e1e1e",
+        darkBg: "#0a0a0a",
+      },
+      fontFamily: {
+        akzidenz: ["'Akzidenz'", "sans-serif"],
+        cormorant: ["'Cormorant'", "serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
