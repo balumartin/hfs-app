1a1eceff0b2aae8a1f97d5b1ce0cedd9773dba71
balumartin <martinka.balazs@gmail.com>
2025-06-10 14:05:51 +0200

    fix News components

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -3,12 +3,14 @@ import Card from "./Card";
 
 export default function News() {
   return (
-    <section className="mx-container mb-10">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {NEWS?.map((n) => (
-          <Card key={n.key} item={n} />
-        ))}
+    <section className="mx-container b-container py-6 px-10 mb-10">
+      <h1 className="mb-5 font-bold text-2xl text-slate-700 border-b border-slate-200">
+        Legfrissebb hírek
+      </h1>
+      <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {NEWS?.map((n, idx) => idx < 3 && <Card key={n.key} item={n} />)}
       </div>
+      <p className="flexEnd text-lg mt-5">További hírek</p>
     </section>
   );
 }
