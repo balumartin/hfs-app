16b2f666e0c92a6a4b2d2de0cbbfcec61277ce51
balumartin <martinka.balazs@gmail.com>
2025-09-10 10:48:21 +0200

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,10 @@
-"use client";
-
-import { useTranslations } from "next-intl";
-
 export default function Footer() {
-  const t = useTranslations("footer");
 
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full h-20 flexCenter bg-black py-4">
-      <p className="text-sm text-slate-300">
-        © {currentYear}{` balumartin. `}{t("title")}
-      </p>
+    <footer className="w-full py-4 text-center text-sm text-gray-500 bg-gray-100">
+      © {new Date().getFullYear()} balumartin
     </footer>
   );
 }
