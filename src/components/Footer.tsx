6381b720a00a44a9b63558b76d3f865b73db48bb
balumartin <martinka.balazs@gmail.com>
2025-09-14 09:00:25 +0200

    add powered by logo, fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,23 @@
-export default function Footer() {
+import Image from "next/image";
 
+export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full py-4 text-center text-sm text-gray-500 bg-gray-100">
-      © {new Date().getFullYear()} balumartin
+    <footer className="relative max-sm:flex-col flexCenter max-sm:gap-4 gap-10 w-full h-20 py-4 text-center text-sm text-gray-500 bg-gray-100">
+      <span>© {new Date().getFullYear()} balumartin</span>
+      <div className="relative flexCenter">
+        <p className="w-full">powered by{" "}</p>
+        <a target="_about" href="https://mome.hu/" className="w-full">
+          <Image
+            src="/mome-logo.svg"
+            alt="mome-logo"
+            width={88}
+            height={22}
+            className="object-cover bg-white invert p-1"
+          />
+        </a>
+      </div>
     </footer>
   );
 }
